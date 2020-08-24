93839b14403aab31b667389620fc6abb27cb990b
Luciana Calvo <lugovlac@gmail.com>
2020-08-24 12:12:14 -0300

    wh

--- webhook.php
+++ webhook.php
@@ -2,8 +2,9 @@
 	http_response_code(200);
 
 	$webhTxt = "webhook.txt";
+	$data = "hola hola hola ohla hola holahola"
 	$json = file_get_contents('php://input');
-	file_put_contents($webhTxt, $json);
+	file_put_contents($webhTxt, $data);
 	// require_once 'vendor/autoload.php';
 
  //    MercadoPago\SDK::setAccessToken("APP_USR-6317427424180639-042414-47e969706991d3a442922b0702a0da44-469485398");
